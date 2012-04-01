d960a1683e58741d69ccae6478cec44a5957155e
unknown <ear7631@REDWYNE.(none)>
2012-03-31 21:20:02 -0400

    kinda works

--- KeyCollectorGUI/KeyCollectorGUI/TestTextControl.xaml.cs
+++ KeyCollectorGUI/KeyCollectorGUI/TestTextControl.xaml.cs
@@ -19,15 +19,28 @@ namespace KeyCollectorGUI
     /// </summary>
     public partial class TestTextControl : UserControl
     {
+        string sampleText = string.Empty;
         string userString = string.Empty;
+        Run building = new Run(string.Empty);
         LinkedList<Run> runs = new LinkedList<Run>();
+
+        Brush Good = Brushes.LightGreen;
+        Brush Bad = Brushes.LightSalmon;
 
         public TestTextControl()
         {
             InitializeComponent();
 
+            // key processing events
             testTextBox.TextInput += new TextCompositionEventHandler(testText_PreviewTextInput);
             testTextBox.PreviewKeyDown += new KeyEventHandler(testTextBox_PreviewKeyDown);
+
+            // get the text the user will type
+            sampleText = untouched.Text;
+
+            building.Background = Good;
+
+            //PWrapper.Inlines.Add(new Run("hello world"));
 
             //this.PreviewKeyDown += new KeyEventHandler(keyPressed);
             /*
@@ -40,32 +53,118 @@ namespace KeyCollectorGUI
             }*/
         }
 
+        protected void updateParagraph()
+        {
+            PWrapper.Inlines.Clear();
+
+            foreach (Run r in runs)
+            {
+                PWrapper.Inlines.Add(r);
+            }
+            PWrapper.Inlines.Add(building);
+            PWrapper.Inlines.Add(untouched);
+        }
+
         protected void processUserString()
         {
             Console.WriteLine(userString);
+            untouched.Text = sampleText.Substring(Math.Min(sampleText.Length, userString.Length));
+            updateParagraph();
+        }
+
+        protected void addCharacter(string c)
+        {
+            userString += c;
+
+            if (userString.Length > sampleText.Length)
+            {
+                // BAD
+                c = " ";
+                if (building.Background == Bad)
+                {
+                    building.Text += c;
+                }
+                else
+                {
+                    runs.AddLast(building);
+                    building = new Run(c);
+                    building.Background = Bad;
+                }
+            }
+            else
+            {
+                c = "" + sampleText[userString.Length - 1];
+                if (userString[userString.Length - 1] == sampleText[userString.Length - 1])
+                {
+                    // GOOD
+                    if (building.Background == Good)
+                    {
+                        building.Text += c;
+                    }
+                    else
+                    {
+                        runs.AddLast(building);
+                        building = new Run(c);
+                        building.Background = Good;
+                    }
+                }
+                else
+                {
+                    // BAD
+                    if (building.Background == Bad)
+                    {
+                        building.Text += c;
+                    }
+                    else
+                    {
+                        runs.AddLast(building);
+                        building = new Run(c);
+                        building.Background = Bad;
+                    }
+                }
+            }
+
+            processUserString();
+        }
+
+        protected void deleteCharacter()
+        {
+            if (userString.Length >= 1)
+            {
+                userString = userString.Substring(0, userString.Length - 1);
+
+                if (building.Text.Length == 0 && runs.Count > 0)
+                {
+                    building = runs.Last();
+                    runs.RemoveLast();
+                }
+
+                if (building.Text.Length > 0)
+                {
+                    building.Text = building.Text.Substring(0, building.Text.Length - 1);
+                }
+
+                processUserString();
+            }
         }
 
         protected void testText_PreviewTextInput(Object sender, TextCompositionEventArgs e)
         {
-            userString += e.Text;
-            processUserString();
+            addCharacter(e.Text);
         }
-
 
         protected void testTextBox_PreviewKeyDown(Object sender, KeyEventArgs e)
         {
             switch (e.Key)
             {
                 case Key.Space:
-                    userString += " ";
-                    processUserString();
+                    addCharacter(" ");
+                    break;
+                case Key.Tab:
+                    addCharacter("\t");
                     break;
                 case Key.Back:
-                    if (userString.Length > 0)
-                    {
-                        userString = userString.Substring(0, -2);
-                        processUserString();
-                    }
+                    deleteCharacter();
                     break;
             }
         }
