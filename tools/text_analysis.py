115e772188fc3d45a4b19df56b83fa2da04e56a0
Eitan Romanoff <ear7631@gmail.com>
2012-04-09 11:25:39 -0400

    Fixed flags, coverage, cleaner code

--- tools/text_analysis.py
+++ tools/text_analysis.py
@@ -1,8 +1,55 @@
 import operator
 import sys
 
+
+def print_dict_sorted(d, zero_only, filter_zero, show_coverage):
+    c = 0
+    for pair in sorted(d.iteritems(), key=operator.itemgetter(1), reverse=True):
+        if pair[1] > 0:
+            c += 1
+        if zero_only and pair[1] > 0:
+            continue
+        if filter_zero and pair[1] == 0:
+            continue
+        print pair
+
+    if show_coverage:
+        p = 100 * c / len(d.values())
+        print "Coverage: " + str(c) + "/" + str(len(d.values())) + " " + str(p) + "%"
+    
+
 if len(sys.argv) < 2:
-    sys.exit("Usage: python text_analysis filename")
+    sys.exit('''\
+Usage: text_analysis [-[options]] filename [flags]
+or more information: text_analysis --help\
+''')
+
+if sys.argv[1] == '--help':
+    print '''\
+Usage: text_analysis [-[options]] filename [flags]
+options
+    d - show digraphs
+    t - show trigraphs
+    r - show doubles
+
+
+flags:
+    --filter-zero   - filter out instances with frequency < 0
+                            (conflicts with --zero-only)
+    --zero-only     - print out only entries with frequency == 0
+                            (conflicts with --filter-zero)
+    --show-coverage - print out a coverage percentage for each stat\
+'''
+
+    sys.exit()
+
+options = sys.argv[2:]
+filter_zero = '--filter-zero' in options
+zero_only = '--zero-only' in options
+show_coverage = '--show-coverage' in options
+
+if filter_zero and zero_only:
+    sys.exit("Error: conflicting flags")
 
 s = ''
 s_upper = ''
@@ -31,7 +78,7 @@ for i in range(65, 91):
 
 common_digraphs = ['TH', 'HE', 'AN', 'IN', 'ER', 'ON', 'RE', 'ED', 'ND', 'HA', 'AT', 'EN', 'ES', 'OF', 'NT', 'EA', 'TI', 'TO', 'IO', 'LE', 'IS', 'OU', 'AR', 'AS', 'DE', 'RT', 'VE']
 common_trigraphs = ['THE', 'AND', 'THA', 'ENT', 'ION', 'TIO', 'FOR', 'NDE', 'HAS', 'NCE', 'TIS', 'OFT', 'MEN']
-common_double = ['SS', 'EE', 'TT', 'FF', 'LL', 'MM', 'OO']
+common_doubles = ['SS', 'EE', 'TT', 'FF', 'LL', 'MM', 'OO']
 
 digraph_freq = {}
 common_digraph_freq = {}
@@ -49,7 +96,7 @@ for trigraph in common_trigraphs:
     common_trigraph_freq[trigraph] = count
 
 print "Checking common doubles..."
-for double in common_double:
+for double in common_doubles:
     count = s_upper.count(double)
     double_freq[double] = count
 
@@ -58,43 +105,18 @@ for digraph in digraphs:
     count = s_upper.count(digraph)
     digraph_freq[digraph] = count
 
-print "Computing letter frequency..."
-for word in s:
-    for letter in word:
-        letter_freq[letter] += 1
-
-print "Done!"
+print "Done"
 
 
-
-# print common digraphs
 print "========Common Digraphs========"
-for pair in sorted(common_digraph_freq.iteritems(), key=operator.itemgetter(1), reverse=True):
-    if pair[1] > 0:
-        print pair
+print_dict_sorted(common_digraph_freq, zero_only, filter_zero, show_coverage)
 
-
-# print common trigraphs
 print "========Common Trigraphs========"
-for pair in sorted(common_trigraph_freq.iteritems(), key=operator.itemgetter(1), reverse=True):
-    if pair[1] > 0:
-        print pair
+print_dict_sorted(common_trigraph_freq, zero_only, filter_zero, show_coverage)
 
-
-# print doubles
 print "========Common Doubles========"
-for pair in sorted(double_freq.iteritems(), key=operator.itemgetter(1), reverse=True):
-    if pair[1] > 0:
-        print pair
+print_dict_sorted(double_freq, zero_only, filter_zero, show_coverage)
 
-
-# print all digraphs
 print "========All Digraphs========"
-for pair in sorted(digraph_freq.iteritems(), key=operator.itemgetter(1), reverse=True):
-    print pair
+print_dict_sorted(digraph_freq, zero_only, filter_zero, show_coverage)
 
-
-# print letter frequency
-print "========Letter Frequency========"
-for pair in sorted(letter_freq.iteritems(), key=operator.itemgetter(1), reverse=True):
-    print pair
